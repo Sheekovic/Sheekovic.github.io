9f972d52ffa03cb77590a8c52848515393ab6044
Sheekovic <ahmeedsheeko@gmail.com>
2024-12-16 01:22:17 +0200

    update

---
+++ assets/js/firebase-config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+    apiKey: "AIzaSyCnzRojHjTeb1e_enXi6YWMse7X6w0vPXo",
+    authDomain: "sheeko-github.firebaseapp.com",
+    projectId: "sheeko-github",
+    storageBucket: "sheeko-github.firebasestorage.app",
+    messagingSenderId: "77160038597",
+    appId: "1:77160038597:web:964aa0a9b4a426ffcc30ac",
+    measurementId: "G-3E4W2ESB42"
+  };
