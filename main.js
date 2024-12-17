96df99b03d93822e826ad995b2d8fcb5cd4651ef
Sheekovic <ahmeedsheeko@gmail.com>
2024-12-17 16:05:49 +0200

    update

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
 // Import Firebase SDKs
+require('dotenv').config();
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-analytics.js";
 import { getAuth, GithubAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js";
 
 // Firebase Configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCnzRojHjTeb1e_enXi6YWMse7X6w0vPXo",
-  authDomain: "sheeko-github.firebaseapp.com",
-  projectId: "sheeko-github",
-  storageBucket: "sheeko-github.firebasestorage.app",
-  messagingSenderId: "77160038597",
-  appId: "1:77160038597:web:964aa0a9b4a426ffcc30ac",
-  measurementId: "G-3E4W2ESB42"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MSG_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
