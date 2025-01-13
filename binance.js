b000f7214960fc0b52a54915c0e1cdf6159b76dc
Sheekovic <78682787+Sheekovic@users.noreply.github.com>
2025-01-13 02:34:14 +0200

    "Refactoring and Adjustments on binance, forum, sidebars"

--- binance.js
+++ binance.js
@@ -1,14 +1,14 @@
 // Firebase Configuration
-import firebaseConfig from './assets/js/firebase-config.js'; 
-import { firebaseApp } from './assets/js/firebase-config.js'; 
+import firebaseConfig from './assets/js/firebase-config.js';
+import { firebaseApp } from './assets/js/firebase-config.js';
 import { getAuth, GithubAuthProvider, signInWithPopup, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-analytics.js";
 
 // Initialize Firebase services
-const auth = getAuth(firebaseApp); 
+const auth = getAuth(firebaseApp);
 const githubProvider = new GithubAuthProvider();
 auth.languageCode = 'en';
-const analytics = getAnalytics(firebaseApp); 
+const analytics = getAnalytics(firebaseApp);
 
 // Initialize Elements
 const githubSignUpButton = document.getElementById('github-signup');
@@ -19,31 +19,31 @@ const registerResult = document.getElementById('register-result');
 
 // Handle Auth State Changes
 onAuthStateChanged(auth, (user) => {
-  if (user) {
-      const userProfilePic = user.photoURL || "default-avatar.png";
-      const userName = user.displayName || "Anonymous";
+    if (user) {
+        const userProfilePic = user.photoURL || "default-avatar.png";
+        const userName = user.displayName || "Anonymous";
 
-      githubSignUpButton.innerHTML = `
+        githubSignUpButton.innerHTML = `
           <img src="${userProfilePic}" alt="Profile Picture" style="width: 24px; height: 24px; border-radius: 50%; margin-right: 8px;">
           <span>${userName}</span>
       `;
-      githubSignUpButton.style.display = "flex";
-      githubSignUpButton.style.alignItems = "center";
+        githubSignUpButton.style.display = "flex";
+        githubSignUpButton.style.alignItems = "center";
 
-      yourUsername.innerText = userName;
-  } else {
-      githubSignUpButton.innerHTML = `
+        yourUsername.innerText = userName;
+    } else {
+        githubSignUpButton.innerHTML = `
           <button id="github-signup" class="btn btn-primary">Sign Up with GitHub</button>
       `;
-      yourUsername.innerText = "Anonymous";
-  }
+        yourUsername.innerText = "Anonymous";
+    }
 });
 
 // GitHub Sign-Up Button Click Event
 githubSignUpButton.addEventListener('click', () => {
-  signInWithPopup(auth, githubProvider)
-      .then(response => updateUI(response.user))
-      .catch(error => console.error("GitHub Sign-Up Error:", error));
+    signInWithPopup(auth, githubProvider)
+        .then(response => updateUI(response.user))
+        .catch(error => console.error("GitHub Sign-Up Error:", error));
 });
 
 // Register Telegram ID
