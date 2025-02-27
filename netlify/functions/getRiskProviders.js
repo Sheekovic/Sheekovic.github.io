e82383b6e4cb245831a80dc00e6a504a25639a75
Sheekovic <78682787+Sheekovic@users.noreply.github.com>
2025-02-27 15:19:27 +0200

    Add serverless function to fetch risk providers from external API

---
+++ netlify/functions/getRiskProviders.js
@@ -0,0 +1,30 @@
+const fetch = require('node-fetch');
+
+exports.handler = async (event, context) => {
+  const response = await fetch('https://notification.blackhawknetwork.com/riskService/v1/riskWidget/getRiskProviders', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json, text/plain, */*',
+          'Referer': 'https://mygift.giftcardmall.com/',
+          'Origin': 'https://mygift.giftcardmall.com',
+          'requestid': '6293b3f1-c990-4aeb-affe-a87d2c273e9a',
+          'unique-id': '6293b3f1-c990-4aeb-affe-a87d2c273e9a',
+          'clientconfigid': 'VNTVQSWJQ54TV27KDCZ7CMH5HW',
+          'sec-fetch-dest': 'empty',
+          'sec-fetch-mode': 'cors',
+          'sec-fetch-site': 'cross-site',
+          'access-control-allow-headers': 'Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, requestId, unique-id, merchantId, tenantId, certificateId, role, clientConfigId',
+          'dnt': '1',
+          'sec-gpc': '1',
+          'te': 'trailers'
+      },
+      body: JSON.stringify({"clientConfigId": "VNTVQSWJQ54TV27KDCZ7CMH5HW"})
+  });
+
+  const data = await response.json();
+  return {
+    statusCode: 200,
+    body: JSON.stringify(data)
+  };
+};
