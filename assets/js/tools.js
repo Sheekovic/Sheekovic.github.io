59d6f38e67c727959b14b15289a67a1d33411eaf
Sheekovic <78682787+Sheekovic@users.noreply.github.com>
2026-02-25 00:11:02 +0200

    Remove removed-tool removed-tool tool and associated HTML file from the project

--- assets/js/tools.js
+++ assets/js/tools.js
@@ -8,7 +8,6 @@ const tools = [
   { file: "acrossboard/app.html", name: "AcrossBoard App", description: "Clipboard-sharing tool for seamless data transfer." },
   { file: "github.html", name: "GitHub Repo Viewer", description: "View your GitHub repositories and user info." },
   { file: "password.html", name: "Personalized Password", description: "Generate a personalized password based on your name and date of birth." },
-
 ];
 
 // Sort tools alphabetically by the first character of the name
